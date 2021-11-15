809fac96c1a262c13cded0e39767ae9e10157f50
aciculachen <am3000520@gmail.com>
2021-11-15 23:10:53 +0800

    2021 navbar sucks

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
-$(".nav-link").on("click", function(){
-
-   $(".nav").find(".active").removeClass("active");
+/*$(".nav-link").on("click", function(){
+   $(".nav ").find(".active").removeClass("active");
    $(this).addClass("active");
 });
+
+
+
+
